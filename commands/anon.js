2f4b11ea6d43de6bb66151d6ec56883b2c359b32
Seligmann <zachary.a.seligman@gmail.com>
2023-03-23 16:29:53 -0500

    slash command for making messages anonymous

---
+++ commands/anon.js
@@ -0,0 +1,18 @@
+const {SlashCommandBuilder} = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('anon')
+        .setDescription('Makes your message anonymous.')
+        .addStringOption(option =>
+            option.setName('message')
+                .setDescription('The message to make anonymous')
+                .setRequired(true)),
+    async execute(interaction, client) {
+        const response = interaction.options.getString('message') ?? 'no string provided';
+        interaction.reply({content: response, ephemeral: true});
+        const channel = await client.channels.fetch(interaction.channelId);
+        await channel.send(response);
+    },
+};
+
