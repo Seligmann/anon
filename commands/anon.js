11f3ca27041611b07d4e1fec2cb23231f79ca217
Seligmann <zachary.a.seligman@gmail.com>
2023-04-15 14:48:08 -0500

    prettier

--- commands/anon.js
+++ commands/anon.js
@@ -1,18 +1,20 @@
-const {SlashCommandBuilder} = require('discord.js');
+const { SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('anon')
-        .setDescription('Makes your message anonymous.')
-        .addStringOption(option =>
-            option.setName('message')
-                .setDescription('The message to make anonymous')
-                .setRequired(true)),
-    async execute(interaction, client) {
-        const response = interaction.options.getString('message') ?? 'no string provided';
-        interaction.reply({content: response, ephemeral: true});
-        const channel = await client.channels.fetch(interaction.channelId);
-        await channel.send(response);
-    },
+  data: new SlashCommandBuilder()
+    .setName("anon")
+    .setDescription("Makes your message anonymous.")
+    .addStringOption((option) =>
+      option
+        .setName("message")
+        .setDescription("The message to make anonymous")
+        .setRequired(true)
+    ),
+  async execute(interaction, client) {
+    const response =
+      interaction.options.getString("message") ?? "no string provided";
+    interaction.reply({ content: response, ephemeral: true });
+    const channel = await client.channels.fetch(interaction.channelId);
+    await channel.send(response);
+  },
 };
-
